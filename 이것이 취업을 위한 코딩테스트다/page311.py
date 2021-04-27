35fd553d90e4d5e9dd6e8fcab7e8b24c7bf51e56
Junhyuk <62590913+deeptwinkle@users.noreply.github.com>
2021-04-27 18:47:52 +0900

    P.311_Q1

---
+++ 이것이 취업을 위한 코딩테스트다/page311.py
@@ -0,0 +1,17 @@
+#page311 Q01
+group_num = 0
+N = int(input())
+fears = list(map(int, input().split()))
+#print(fears)
+fears.sort(reverse=True)
+#print(fears)
+for i in range(N):
+    group = list()
+    mx = fears[i]
+    group.append(fears[i:i+mx])
+    if i+mx < N:
+        group.append(fears[i+mx:])
+    gr_len = len(group)
+    if gr_len > group_num:
+        group_num = gr_len
+print(group_num)
